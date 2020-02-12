03515272cce3bf49a9f636569ee2a8a7ed3351dc
Luca Pasquali <luca.pasquali.91@gmail.com>
2020-02-12 14:06:53 +0100

    Docs: added docstring for distance

--- molpy/util.py
+++ molpy/util.py
@@ -1,6 +1,21 @@
 import numpy as np
 
 def distance(point1, point2):
+    """
+    Returns the distance between two points
+
+    Parameters
+    ----------
+    point1: array_like
+        First point
+    point2: array_like
+        Second point
+
+    Returns
+    -------
+    float
+        The distance between the two points
+    """
     a = np.asarray(point1)
     b = np.asarray(point2)
     retval = np.linalg.norm(a -  b)
