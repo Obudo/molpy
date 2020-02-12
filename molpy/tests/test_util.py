fcb1ac239737a2c088712f5f9a873d55d63b2bc2
Luca Pasquali <luca.pasquali.91@gmail.com>
2020-02-12 13:49:12 +0100

    Test: implemented parametrized pytest tests

--- molpy/tests/test_util.py
+++ molpy/tests/test_util.py
@@ -2,6 +2,9 @@
 import molpy
 import pytest
 
-def test_distance():
-    assert molpy.util.distance([0,1], [0,0]) == 1
-    assert molpy.util.distance([1,0,1,0], [0,1,0,1]) == 2
+@pytest.mark.parametrize("point1,point2,bench", [
+    ([0,1], [0,0], 1),
+    ([1,0,1,0], [0,1,0,1], 2),
+])
+def test_distance(point1, point2, bench):
+    assert molpy.util.distance(point1, point2) == bench
