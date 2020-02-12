ae647b3096f329a930f0cf735401c1976cca40bd
Luca Pasquali <luca.pasquali.91@gmail.com>
2020-02-12 13:35:41 +0100

    Test: added distance test

---
+++ molpy/tests/test_util.py
@@ -0,0 +1,6 @@
+# Import package, test suite, and other packages as needed
+import molpy
+import pytest
+
+def test_distance():
+    assert molpy.util.distance([0,1], [0,0]) == 1
--- molpy/util.py
+++ molpy/util.py
@@ -1,5 +1,7 @@
 import numpy as np
 
 def distance(point1, point2):
-    retval = np.linalg.norm(point1, point2)
+    a = np.asarray(point1)
+    b = np.asarray(point2)
+    retval = np.linalg.norm(a -  b)
     return retval
