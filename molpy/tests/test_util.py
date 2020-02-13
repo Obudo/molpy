1978ac3cd8b27a904bda50ea4f2c31be915c09ac
Luca Pasquali <luca.pasquali.91@gmail.com>
2020-02-13 10:02:07 +0100

    Code Formatting: ran first yapf

--- molpy/tests/test_util.py
+++ molpy/tests/test_util.py
@@ -2,13 +2,15 @@
 from molpy.util import distance as dist
 import pytest
 
+
 @pytest.mark.parametrize("point1,point2,bench", [
-    ([0,1], [0,0], 1),
-    ([1,0,1,0], [0,1,0,1], 2),
+    ([0, 1], [0, 0], 1),
+    ([1, 0, 1, 0], [0, 1, 0, 1], 2),
     ([2.236, 2.236], [0, 0], 3.162),
 ])
 def test_distance(point1, point2, bench):
     assert dist(point1, point2) == pytest.approx(bench, abs=1e-3)
 
+
 def test_distance_failure():
-    assert dist([0], [3]) != 5 
+    assert dist([0], [3]) != 5
--- molpy/util.py
+++ molpy/util.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def distance(point1, point2):
     """
     Returns the distance between two points
@@ -18,12 +19,13 @@ def distance(point1, point2):
     """
     a = np.asarray(point1)
     b = np.asarray(point2)
-    retval = np.linalg.norm(a -  b)
+    retval = np.linalg.norm(a - b)
     return retval
+
 
 def open_xyz(file_location):
     xyz_file = np.genfromtxt(fname=file_location, skip_header=2, dtype='unicode')
-    symbols = xyz_file[:,0]
-    coords = (xyz_file[:,1:])
+    symbols = xyz_file[:, 0]
+    coords = (xyz_file[:, 1:])
     coords = coords.astype(np.float)
     return symbols, coords
