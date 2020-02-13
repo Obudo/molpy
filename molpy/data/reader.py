b633ab24c9513b9e5ffdf4c54bfe372632b64d71
Luca Pasquali <luca.pasquali.91@gmail.com>
2020-02-13 09:36:45 +0100

    Imports fix: changed util import to relative

--- molpy/data/reader.py
+++ molpy/data/reader.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from molpy.util import open_xyz
+from ..util import open_xyz
 
 dirname = os.path.dirname(os.path.abspath(__file__))
 filename = os.path.join(dirname, 'look_and_say.dat')
